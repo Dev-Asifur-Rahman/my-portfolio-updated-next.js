b4cf6158582b5edf416a513ede0638b86ccf215c
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-09 23:50:00 +0600

    background added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="relative">
-      <button className="btn bg-amber-400">Default</button>
+      <button className="btn">Default</button>
     </div>
   );
 }
