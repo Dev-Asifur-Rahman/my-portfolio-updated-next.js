6787e8a021a60d087cd44c291a6cb3d9b952032d
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-10 23:50:00 +0600

    title logo added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="relative">
-      <button className="btn">Default</button>
+      <button className="btn matt-glass">Trigger</button>
     </div>
   );
 }
