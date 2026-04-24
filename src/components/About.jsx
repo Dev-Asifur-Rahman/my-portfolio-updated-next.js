08758d8b2ffd2c94f521d66e1373b92a66881f15
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-24 23:50:00 +0600

    about section spacing set

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div id='about' className='w-full h-screen glass-bg'>
+        <div id='about' className='w-full lg:mt-10 md:mt-6 mt-4 rounded-lg md:rounded-xl h-screen glass-bg'>
             About
         </div>
     );
