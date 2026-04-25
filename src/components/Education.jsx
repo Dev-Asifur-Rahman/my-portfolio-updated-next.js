8a4ace4860cd54f22bc326354a1e9611e2183feb
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-25 23:50:00 +0600

    margin set on top of education component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Education = () => {
     return (
-        <div id='education' className='w-full h-screen glass-bg'>
+        <div id='education' className='w-full lg:mt-10 md:mt-6 mt-4 rounded-lg md:rounded-xl h-screen glass-bg'>
             Education
         </div>
     );
