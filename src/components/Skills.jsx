485a033c3ded37bc115cab3424f5ec5e4b2441a4
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-27 23:50:00 +0600

    margin set on skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div id='skills' className='w-full h-screen glass-bg'>
+        <div id='skills' className='w-full lg:mt-10 md:mt-6 mt-4 rounded-lg md:rounded-xl h-screen glass-bg'>
             Skills
         </div>
     );
