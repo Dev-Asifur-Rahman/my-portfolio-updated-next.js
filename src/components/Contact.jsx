9bf66c116add54b501f3a74384afe3eb14c89fa4
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-04-28 23:50:00 +0600

    margin set on contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div id='contact' className='w-full h-screen glass-bg'>
+        <div id='contact' className='w-full lg:mt-10 md:mt-6 mt-4 rounded-lg md:rounded-xl h-screen glass-bg'>
             Contact
         </div>
     );
