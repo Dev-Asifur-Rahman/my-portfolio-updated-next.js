a29eff8922b3f85bd849f1952412f04b47434e9f
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-05-01 23:50:00 +0600

    navbar scalling with nav added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 
 const NavBar = ({ scrollRef }) => {
   const [progress, setProgress] = useState(0);
+  const [isScrolled, setIsScrolled] = useState(false);
+
   useEffect(() => {
     const element = scrollRef.current;
     if (!element) return;
@@ -13,8 +15,17 @@ const NavBar = ({ scrollRef }) => {
       const scrollHeight = element.scrollHeight;
       const clientHeight = element.clientHeight;
 
-      const percent = (scrollTop / (scrollHeight - clientHeight)) * 100;
+      // progress bar
+      const maxScroll = scrollHeight - clientHeight;
+      const percent = maxScroll > 0 ? (scrollTop / maxScroll) * 100 : 0;
       setProgress(percent);
+
+      // navbar scale logic
+      if (scrollTop < 5) {
+        setIsScrolled(false);
+      } else {
+        setIsScrolled(true);
+      }
     };
 
     handleScroll();
@@ -22,10 +33,22 @@ const NavBar = ({ scrollRef }) => {
     element.addEventListener("scroll", handleScroll);
     return () => element.removeEventListener("scroll", handleScroll);
   }, [scrollRef]);
+
   return (
     <div
       id="navbar"
-      className="glass-bg backdrop-blur-xl h-12 md:h-16 lg:h-[76px]  flex justify-between items-center mb-2 md:mb-3 lg:mb-4 md:rounded-xl rounded-lg  p-2 md:p-3 lg:p-4 sticky top-0 z-50"
+      className={`
+        glass-bg backdrop-blur-xl
+        h-12 md:h-16 lg:h-[76px]
+        flex justify-between items-center
+        mb-2 md:mb-3 lg:mb-4
+        md:rounded-xl rounded-lg
+        p-2 md:p-3 lg:p-4
+        sticky top-0 z-50
+
+        transition-all duration-300 ease-in-out origin-top
+        ${isScrolled ? "scale-95" : "scale-100"}
+      `}
     >
       <div className="flex items-center gap-2 w-2/3 ">
         <Image
@@ -34,20 +57,24 @@ const NavBar = ({ scrollRef }) => {
           height={500}
           alt="asifurs-image-logo"
           className="w-5 md:w-8 lg:w-10 aspect-square"
-        ></Image>
+        />
+
         <p className="text-xl md:text-3xl lg:text-4xl font-bold text-gradient">
           Asifur Rahman
         </p>
       </div>
+
       <div className="w-1/3">
         <progress
-          className="progress [&::-webkit-progress-value]:bg-gradient-to-r
-  [&::-webkit-progress-value]:from-[#38bdf8]
-  [&::-webkit-progress-value]:via-[#6366f1]
-  [&::-webkit-progress-value]:to-[#a855f7] w-full"
+          className="progress transition-all duration-200
+          [&::-webkit-progress-value]:bg-gradient-to-r
+          [&::-webkit-progress-value]:from-[#38bdf8]
+          [&::-webkit-progress-value]:via-[#6366f1]
+          [&::-webkit-progress-value]:to-[#a855f7]
+          w-full"
           value={progress}
           max="100"
-        ></progress>
+        />
       </div>
     </div>
   );
