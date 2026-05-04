8739a59b908c2420caa8e7fbcdac77d3b1befb86
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-05-04 23:50:00 +0600

    about section cards turned into object based and added animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,32 @@ import React from "react";
 import { Code2, Database, Globe, LayoutDashboard } from "lucide-react";
 
 const About = () => {
+  const aboutCard = [
+    {
+      logo: <Code2 className="text-white mb-4" size={34} />,
+      title: "Frontend Development",
+      description:
+        "Building responsive and interactive user interfaces using React.js, Next.js and Tailwind CSS.",
+    },
+    {
+      logo: <Database className="text-white mb-4" size={34} />,
+      title: "Backend Development",
+      description:
+        "Creating scalable server-side applications using Node.js, Express.js and MongoDB.",
+    },
+    {
+      logo: <LayoutDashboard className="text-white mb-4" size={34} />,
+      title: "UI/UX Focused",
+      description:
+        "Designing smooth and visually appealing experiences with modern, layouts and animations.",
+    },
+    {
+      logo: <Globe className="text-white mb-4" size={34} />,
+      title: "Web Technologies",
+      description:
+        "Working with modern tools, APIs, authentication systems and deployment platforms.",
+    },
+  ];
   return (
     <section
       id="about"
@@ -18,11 +44,11 @@ const About = () => {
           </h2>
 
           <p className="mt-5 text-white/70 text-sm md:text-base lg:text-lg leading-relaxed max-w-4xl">
-            A <span className="text-gradient">Full Stack Developer</span> passionate about creating
-            modern, responsive and user-focused web applications. I enjoy
-            turning ideas into interactive digital experiences using
-            technologies like React, Next.js, Typescript, Node.js, Express.js
-            and MongoDB, PostgreSQL.
+            A <span className="text-gradient ">Full Stack Developer</span>{" "}
+            passionate about creating modern, responsive and user-focused web
+            applications. I enjoy turning ideas into interactive digital
+            experiences using technologies like React, Next.js, Typescript,
+            Node.js, Express.js and MongoDB, PostgreSQL.
           </p>
 
           <p className="mt-4 text-white/70 text-sm md:text-base lg:text-lg leading-relaxed max-w-4xl">
@@ -33,53 +59,19 @@ const About = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
-          <div className="glass-bg rounded-xl p-5 border border-white/10">
-            <Code2 className="text-white mb-4" size={34} />
-
-            <h3 className="text-xl font-bold text-white">
-              Frontend Development
-            </h3>
-
-            <p className="mt-3 text-white/70 text-sm leading-relaxed">
-              Building responsive and interactive user interfaces using
-              React.js, Next.js and Tailwind CSS.
-            </p>
-          </div>
-
-          <div className="glass-bg rounded-xl p-5 border border-white/10">
-            <Database className="text-white mb-4" size={34} />
-
-            <h3 className="text-xl font-bold text-white">
-              Backend Development
-            </h3>
-
-            <p className="mt-3 text-white/70 text-sm leading-relaxed">
-              Creating scalable server-side applications using Node.js,
-              Express.js and MongoDB.
-            </p>
-          </div>
-
-          <div className="glass-bg rounded-xl p-5 border border-white/10">
-            <LayoutDashboard className="text-white mb-4" size={34} />
-
-            <h3 className="text-xl font-bold text-white">UI/UX Focused</h3>
-
-            <p className="mt-3 text-white/70 text-sm leading-relaxed">
-              Designing smooth and visually appealing experiences with modern
-              layouts and animations.
-            </p>
-          </div>
-
-          <div className="glass-bg rounded-xl p-5 border border-white/10">
-            <Globe className="text-white mb-4" size={34} />
-
-            <h3 className="text-xl font-bold text-white">Web Technologies</h3>
-
-            <p className="mt-3 text-white/70 text-sm leading-relaxed">
-              Working with modern tools, APIs, authentication systems and
-              deployment platforms.
-            </p>
-          </div>
+          {aboutCard?.map((card, index) => {
+            return (
+              <div key={index} className="glass-bg rounded-xl p-5 border border-white/10 hover:scale-105 transition-all duration-200">
+                {card?.logo}
+                <h3 className="text-xl font-bold text-white">
+                  {card?.title}
+                </h3>
+                <p className="mt-3 text-white/70 text-sm leading-relaxed">
+                  {card?.description}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
