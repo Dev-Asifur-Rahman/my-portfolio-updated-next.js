7b3d273708c4fd7fd54bf8ca9207c748152e9889
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-05-24 23:12:13 +0600

    onloadingcompleted replaced by onload in hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -87,7 +87,7 @@ const Hero = () => {
                 width={500}
                 height={500}
                 alt="Asifur Rahman"
-                onLoadingComplete={()=> setImageLoaded(true)}
+                onLoad={() => setImageLoaded(true)}
                 className="relative -z-10 w-52 md:w-72 lg:w-[320px] aspect-square object-cover rounded-full"
               />
             </div>
